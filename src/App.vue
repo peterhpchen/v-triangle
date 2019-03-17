21fdfb17c5a5777df57b9dfd5226ec1fc173100e
peterhpchen <peterhsinpingchen@gmail.com>
2019-03-17 23:51:29 +0800

    add demo and triangle component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="vue-icon">
+      <Triangle
+        class="vue-icon__triangle"
+        height="4.375rem"
+        width="5rem"
+        direction="down"
+        color="#41b883"
+      />
+      <Triangle
+        class="vue-icon__triangle"
+        height="2.625rem"
+        width="3rem"
+        direction="down"
+        color="#354951"
+      />
+      <Triangle
+        class="vue-icon__triangle"
+        height="0.875rem"
+        width="1rem"
+        direction="down"
+        color="#ffffff"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Triangle from './components/Triangle.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Triangle
+  },
+  data() {
+    return {
+      height: '1rem',
+      width: '1rem',
+      direction: 'up',
+      color: 'black'
+    };
   }
 }
 </script>
@@ -24,5 +53,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.vue-icon {
+  display: flex;
+  justify-content: center;
+}
+
+.vue-icon__triangle {
+  position: absolute;
 }
 </style>
---
+++ src/components/Triangle.vue
@@ -0,0 +1,86 @@
+<template>
+  <div
+    :style="style"
+  />
+</template>
+
+<script>
+export default {
+  name: 'VTriangle',
+  props: {
+    height: {
+      type: String,
+      required: true,
+      validator(value) {
+        const validateRe = /^(\d*\.?\d*)(ch|em|ex|rem|vh|vw|vmin|vmax|px|cm|mm|in|pc|pt)$/;
+        return !!validateRe.exec(value);
+      },
+    },
+    width: {
+      type: String,
+      required: true,
+      validator(value) {
+        const validateRe = /^(\d*\.?\d*)(ch|em|ex|rem|vh|vw|vmin|vmax|px|cm|mm|in|pc|pt)$/;
+        return !!validateRe.exec(value);
+      },
+    },
+    direction: {
+      type: String,
+      default: 'up',
+      validator(value) {
+        const directionTypes = ['up', 'down', 'left', 'right'];
+        return directionTypes.includes(value);
+      },
+    },
+    color: {
+      type: String,
+      default: 'black',
+    },
+  },
+  computed: {
+    style() {
+      const { height, width, color } = this;
+      const widthRe = /^(\d*\.?\d*)(ch|em|ex|rem|vh|vw|vmin|vmax|px|cm|mm|in|pc|pt)$/;
+      const widthReResult = widthRe.exec(width);
+      const widthNumber = widthReResult[1] / 2; // split equally to two border
+      const widthUnit = widthReResult[2];
+
+      const result = {
+        width: 0,
+        height: 0,
+      };
+
+      if (this.direction === 'up') {
+        return {
+          ...result,
+          'border-bottom': `${height} ${color} solid`,
+          'border-left': `${widthNumber}${widthUnit} transparent solid`,
+          'border-right': `${widthNumber}${widthUnit} transparent solid`,
+        };
+      }
+      if (this.direction === 'down') {
+        return {
+          ...result,
+          'border-top': `${height} ${color} solid`,
+          'border-left': `${widthNumber}${widthUnit} transparent solid`,
+          'border-right': `${widthNumber}${widthUnit} transparent solid`,
+        };
+      }
+      if (this.direction === 'left') {
+        return {
+          ...result,
+          'border-right': `${height} ${color} solid`,
+          'border-top': `${widthNumber}${widthUnit} transparent solid`,
+          'border-bottom': `${widthNumber}${widthUnit} transparent solid`,
+        };
+      }
+      return { // right
+        ...result,
+        'border-left': `${height} ${color} solid`,
+        'border-top': `${widthNumber}${widthUnit} transparent solid`,
+        'border-bottom': `${widthNumber}${widthUnit} transparent solid`,
+      };
+    },
+  },
+};
+</script>
